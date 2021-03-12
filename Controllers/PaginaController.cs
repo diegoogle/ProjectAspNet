5f85a7cc2e9d87545425c7bc815173aef3fcba2e
Diego <48544269+diegoogle@users.noreply.github.com>
2021-03-11 20:04:17 -0600

    Add files via upload

--- Controllers/PaginaController.cs
+++ Controllers/PaginaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiPrimeraAplicacionWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,61 @@ namespace MiPrimeraAplicacionWeb.Controllers
         // GET: Pagina
         public ActionResult Index()
         {
-            return View();
+            List<PaginaCLS> listaPagina = new List<PaginaCLS>();
+            using (var bd = new BDPasajeEntities()) {
+                listaPagina = (from pagina in bd.Pagina
+                               where pagina.BHABILITADO == 1
+                               select new PaginaCLS {
+                                   iidpagina = pagina.IIDPAGINA,
+                                   mensaje = pagina.MENSAJE,
+                                   controlador = pagina.CONTROLADOR,
+                                   accion = pagina.ACCION
+                               }).ToList();
+            }
+                return View(listaPagina);
+        }
+        public ActionResult Filtrar (PaginaCLS paginaCLS) {
+            string mensaje = paginaCLS.mensaje;
+            List<PaginaCLS> listaPagina = new List<PaginaCLS>();
+            using (var bd = new BDPasajeEntities()) {
+                if (mensaje == null) {
+                    
+                    listaPagina = (from pagina in bd.Pagina
+                                   where pagina.BHABILITADO == 1
+                                   select new PaginaCLS {
+                                       iidpagina = pagina.IIDPAGINA,
+                                       mensaje = pagina.MENSAJE,
+                                       controlador = pagina.CONTROLADOR,
+                                       accion = pagina.ACCION
+                                   }).ToList();
+                } else {
+                    listaPagina = (from pagina in bd.Pagina
+                                   where pagina.BHABILITADO == 1
+                                   && pagina.MENSAJE.Contains(mensaje)
+                                   select new PaginaCLS {
+                                       iidpagina = pagina.IIDPAGINA,
+                                       mensaje = pagina.MENSAJE,
+                                       controlador = pagina.CONTROLADOR,
+                                       accion = pagina.ACCION
+                                   }).ToList();
+                }
+            }
+            return PartialView("_TablaPagina", listaPagina);
+        }
+        public int Guardar (PaginaCLS paginaCLS, int titulo) {
+            int respuesta = 0;
+            using (var bd = new BDPasajeEntities()) {
+                if (titulo == 1) {
+                    Pagina pagina = new Pagina();
+                    pagina.MENSAJE = paginaCLS.mensaje;
+                    pagina.ACCION = paginaCLS.accion;
+                    pagina.CONTROLADOR = paginaCLS.controlador;
+                    pagina.BHABILITADO = 1;
+                    bd.Pagina.Add(pagina);
+                    respuesta = bd.SaveChanges();
+                }
+            }
+            return respuesta;
         }
     }
 }
--- Controllers/RolController.cs
+++ Controllers/RolController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiPrimeraAplicacionWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,60 @@ namespace MiPrimeraAplicacionWeb.Controllers
         // GET: Rol
         public ActionResult Index()
         {
-            return View();
+            List<RolCLS> listaRol = new List<RolCLS>();
+            using (var bd = new BDPasajeEntities()) {
+                listaRol = (from rol in bd.Rol
+                            where rol.BHABILITADO == 1
+                            select new RolCLS {
+                                iidRol = rol.IIDROL,
+                                nombre = rol.NOMBRE,
+                                descripcion = rol.DESCRIPCION
+                            }).ToList();
+            }
+            return View(listaRol);
         }
+        public ActionResult Filtro (string nombre) {
+            List<RolCLS> listaRol = new List<RolCLS>();
+            using (var bd = new BDPasajeEntities()) {
+                if (nombre == null) {
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1
+                                select new RolCLS {
+                                    iidRol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+                                }).ToList();
+                } else {
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1 &&
+                                rol.NOMBRE.Contains(nombre)
+                                select new RolCLS {
+                                    iidRol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+                                }).ToList();
+                }
+
+                
+            }
+            return PartialView("_TablaRol", listaRol);
+        }
+        public int Guardar (RolCLS rolCLS, int titulo) {
+            int respuesta = 0;
+            using (var bd = new BDPasajeEntities()) {
+                if (titulo.Equals(1)) {
+                    Rol rol = new Rol();
+                    rol.NOMBRE = rolCLS.nombre;
+                    rol.DESCRIPCION = rolCLS.descripcion;
+                    rol.BHABILITADO = 1;
+                    bd.Rol.Add(rol);
+                    respuesta = bd.SaveChanges();//Devuelve el numero de filas afectadas.
+                }
+            }
+            return respuesta;
+        }
+
+
     }
+
 }
